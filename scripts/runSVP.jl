0711a695ec8772cd54f5c0a8dd9625e00a2418ce
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-10-11 13:34:44 +0200

    CoM difference update for barry

--- scripts/runSVP.jl
+++ scripts/runSVP.jl
@@ -10,7 +10,13 @@ for k in keys(inp)
   ts = svp.readHess(inp[k]["ts"])
   pr = svp.readHess(inp[k]["product"])
   
-  svp.printSVP(re, ts, pr) |> println
+  if haskey(inp[k], "molecule") && haskey(inp[k], "surface")
+    mol  = inp[k]["molecule"]
+    surf = inp[k]["surface"]
+    svp.printSVP(re, ts, pr, mol=mol, surf=surf) |> println
+  else
+    svp.printSVP(re, ts, pr) |> println
+  end
 
   println("End $(k)\n")
 end
--- src/output.jl
+++ src/output.jl
@@ -1,5 +1,11 @@
 
-function printSVP(reac, ts, prod)
+function CoM(bdys)
+  M = sum([i.m for i in bdys])
+  r = sum([i.m*i.r for i in bdys])
+  return r ./ M
+end
+
+function printSVP(reac, ts, prod; mol=(), surf=())
   io = IOBuffer()
 
   rc = let 
@@ -43,6 +49,23 @@ function printSVP(reac, ts, prod)
     p = dot(a[1], rc) |> abs |> (x -> round(x, digits=5))
     println(io, "  $(a[2])      $(p)")
   end
+
+  if isempty(mol) || isempty(surf)
+    return take!(io) |> String
+  end
+
+  println(io, "\n")
+  println(io, "CoM Difference Component")
+  println(io, "------------------------")
+
+  molCoM  = CoM(prod.bdys[mol[1]:mol[2]])
+  surfCoM = CoM(prod.bdys[surf[1]:surf[2]])
+  
+  v = molCoM - surfCoM |> (x -> repeat(x, N))  |> (x -> x / norm(x))
+
+  p = dot(v, rc) |> abs |> (x -> round(x, digits=5))
+
+  println(io, "SVP = $(p)")
 
   take!(io) |> String
 end
